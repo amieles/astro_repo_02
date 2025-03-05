75344542e5dfbed734ec66048a19295732909c58
amieles <96407411+amieles@users.noreply.github.com>
2025-03-05 01:40:43 +0000

    retry time

--- dags/example_test_query.py
+++ dags/example_test_query.py
@@ -9,7 +9,7 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
-    "retry_delay": timedelta(minutes=5),
+    "retry_delay": timedelta(seconds=5),
 }
 @dag(
     "example_test_query",
