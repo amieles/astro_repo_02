cdbcff15f287c7b6db7a0ab7b1cc5525855df20c
amieles <96407411+amieles@users.noreply.github.com>
2025-03-05 01:41:57 +0000

    limit test

--- dags/example_test_query.py
+++ dags/example_test_query.py
@@ -26,7 +26,7 @@ def call_snowflake_scripts():
         conn_id="snowflake_conn",
         sql="test_query.sql",
         show_return_value_in_logs=True,
-        params={"limit_value": 10},  # Parameterizing the limit
+        params={"limit_value": 5},  # Parameterizing the limit
     )
     opr_call_script
 
